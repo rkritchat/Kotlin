ed26dffad9add9859e65e00ee0158704c8ea7478
rkritchat <rkritchat@gmail.com>
2018-11-20 21:01:38 +0700

    Create Variable.kt for begin learning Kotlin

---
+++ src/variable/Variable.kt
@@ -0,0 +1,35 @@
+package variable
+
+fun main(args: Array<String>){
+
+    /**
+     * This is example of declare normal variable
+     * We can declare variable without type
+     */
+    var ten = 10                //Int
+    var name = "kritchat"       //String
+    var weight = 68.5           //Float
+    // ten = "a"                //Will error because a already reserve to Int
+
+    /**
+     * We can also fix type of variable following below
+     */
+    var age : Int               // Declare age to type Int
+    age = 10                    // Assign value 10 to age
+    var lastName : String = "b"  //This is way to fix data type,
+    // if not fix Kotlin will read from value such as "s" -> should be type String
+
+    /**
+     * val is constant and constant cannot be change anymore
+     * val same like []static final] in java
+     */
+    val errorCode = "A0001"     //String
+    // errorCode = "2"  //Kotlin not allow to change constant
+
+    println(ten)
+    println(name)
+    println(weight)
+    println(errorCode)
+    println(lastName)
+    println(age)
+}
