369d1f7deeb495f398dd849c8d6d2b50e6efc103
rkritchat <rkritchat@gmail.com>
2018-11-20 21:53:40 +0700

    Create class with parameter and add about show string

--- src/clazz/Class.kt
+++ src/clazz/Class.kt
@@ -7,6 +7,11 @@ fun main(args : Array<String>){
 
     var externalMonitorObj = ExternalScreen() //Declare variable externalMonitorObj with external class
     externalMonitorObj.show("Love")     //Call method show inside ExternalScreen class
+
+
+    var screenWithParam = ScreenWithParam()
+    screenWithParam.tmp = "Assign data to tmp inside ScreenWithParam class"   //Modify property inside ScreenWithParam class
+    screenWithParam.show()
 }
 
 
@@ -16,5 +21,20 @@ fun main(args : Array<String>){
 class Screen{
     fun show(data : String){
         println(data)
+    }
+}
+
+
+/**
+ * This is example how to create class with property
+ */
+class ScreenWithParam{
+
+    var tmp : String=""                      //property need to initialize
+    //var tmp : String     Kotlin not allow to create property without initialize
+
+
+    fun show(){
+        println(tmp)
     }
 }
--- src/clazz/ExternalClass.kt
+++ src/clazz/ExternalClass.kt
@@ -4,5 +4,11 @@ class ExternalScreen{                       //Class name no need to match with f
 
     fun show(data : String){
         println("External : $data ")        //We can user $ for read variable instead of concat
+        //In case class we need to add bracket such as ${}, if not Kotlin will print address instead value
+        /*  var person = Person()
+         *  person.name = "Kritchat"
+         *  println("person is ${person.name}")   //System will return person is Kritchat
+         *  println("person is $person.name")     //System will return Person@ceca345.name
+         */
     }
 }
